af7fd9b32939fd6e70eb70940d889bd652a14425
Dixser <daniom93@gmail.com>
2024-10-22 14:38:02 +0200

    part 4.a.2

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { mongoUrl } = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+
+module.exports = app
